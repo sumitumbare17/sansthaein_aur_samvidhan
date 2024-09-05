ac17c968717015a1d38412288d07961da942f4fc
SUMIT UMBARE <110318200+sumitumbare17@users.noreply.github.com>
2024-09-05 19:34:17 +0530

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -2,9 +2,9 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 # Path to your Firebase service account key JSON file
-cred = credentials.Certificate('static/iotcp-acf25-firebase-adminsdk-5n7bg-92c5cb8ec4.json')
+cred = credentials.Certificate('Your Key')
 
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://iotcp-acf25-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    'databaseURL': 'Your app link'
 })
